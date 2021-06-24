b1f04637ff993f81c4737d06287577cb19795c3f
asterchang96 <asterchang96@gmail.com>
2021-06-24 10:44:05 +0800

    綁定新增訂單的事件(取得店員選擇的飲料品項、甜度和冰塊)

--- main.js
+++ main.js
@@ -1,8 +1,36 @@
+const alphaPos = new AlphaPos()
+
+
+
 function Drink(name, sugar, ice){ //飲料物件產生
   this.name = name
   this.sugar = sugar
   this.ice = ice
 }
+
+const addDrinkButton = document.querySelector('[data-alpha-pos="add-drink"]')
+addDrinkButton.addEventListener('click', function(){
+  // 1. 取得店員選擇的飲料品項、甜度和冰塊
+  const drinkName = alphaPos.getCheckedValue('drink')
+  const ice = alphaPos.getCheckedValue('ice')
+  const sugar = alphaPos.getCheckedValue('sugar')
+  console.log(`${drinkName}, ${ice}, ${sugar}`)
+  // 2. 如果沒有選擇飲料品項，跳出提示
+  // 3. 建立飲料實例，並取得飲料價格
+  // 4. 將飲料實例產生成左側訂單區的畫面
+})
+
+function AlphaPos () { }
+AlphaPos.prototype.getCheckedValue = function(inputName){
+  let selectOption = ''
+  document.querySelectorAll(`[name=${inputName}]`).forEach(function(item){
+    if(item.checked){
+      selectOption = item.value
+    }
+  })
+  return selectOption
+}
+
 
 Drink.prototype.price = function(){ //取得飲料價格放入Drink中
   switch(this.name){
@@ -21,10 +49,3 @@ Drink.prototype.price = function(){ //取得飲料價格放入Drink中
       alert('No this drink')
   }
 }
-
-
-
-let blackTea = new Drink('Black Tea', 'Half Sugar', 'No Ice')
-console.log(blackTea)
-console.log(blackTea.price())
-
